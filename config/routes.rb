818c795d22ecd7a3a2989ab031f6c33cf12c2123
Conrad Esche <conrad.3rd@gmail.com>
2022-08-17 19:32:54 +0200

    restaurant CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,28 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants do
-    resources :reviews
-  end
+  # root 'restaurants#index'
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show' #, as: :restaurant
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # get 'reviews/index', to: 'reviews#index'
+  # get 'reviews/new', to: 'reviews#new'
+  # get 'reviews/show', to: 'reviews#show'
+  # post 'reviews/create', to: 'reviews#create'
+  # get 'reviews/edit', to: 'reviews#edit'
+  # patch 'reviews/update', to: 'reviews#update'
+  # delete 'reviews/destroy', to: 'reviews#destroy'
+
+  # root 'restaurants#index'
+  resources :restaurants
+      # resources :reviews, only: %i[index new create show]
+
+      # resources :restaurants, only: [:index, :show]
+  # resources :reviews, only: [:index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '23989283', category: 'Fastfood' }
+pizza_east = { name: 'Pizza East', address: '123 Fake Street', phone_number: '23989283', category: 'Fastfood' }
+pizza_hut = { name: 'Pizza Hut', address: '030 Berlin', phone_number: '5232366', category: 'Fastfood' }
+subway = { name: 'Subway', address: '123 Berlin', phone_number: '19764', category: 'Fastfood' }
+burger = { name: 'Burger Burger', address: '432 Berlin', phone_number: '43322', category: 'Fastfood' }
+
+[dishoom, pizza_east, pizza_hut, subway, burger].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
